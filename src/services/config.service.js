f99bbadf0650feb6ad74dc0086d024b55d1447ef
LG972022 <liam.gill@method-analytics.com>
2022-09-21 11:43:30 +0100

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,8 +1,9 @@
-angular.module('app').factory('ConfigService', [function() {
-
+angular.module("app").factory("ConfigService", [
+  function () {
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = "http://quiz-api-l.eu-west-2.elasticbeanstalk.com/";
 
     return service;
-}]);
+  },
+]);
