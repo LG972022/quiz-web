abbb00f359d0dcd7f6cabf2e62b4bf2adc6d8d4f
LG972022 <liam.gill@method-analytics.com>
2022-09-21 14:16:43 +0100

    changed endpoint for api

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module("app").factory("ConfigService", [
   function () {
     var service = {};
 
-    service.apiBase = "http://quiz-api-l.eu-west-2.elasticbeanstalk.com/";
+    service.apiBase = "https://quiz-api-l.eu-west-2.elasticbeanstalk.com/";
 
     return service;
   },
